d839896bcd80beb0e08ba860e9d157919e9ba123
yuki-snow1823 <magic.iroha@gmail.com>
2020-10-18 23:52:32 +0900

    add:todoマイグレーションの確定

--- database/migrations/2020_10_18_144613_create_todos_table.php
+++ database/migrations/2020_10_18_144613_create_todos_table.php
@@ -15,6 +15,8 @@ class CreateTodosTable extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->string("title");
+            $table->string("context");
             $table->timestamps();
         });
     }
