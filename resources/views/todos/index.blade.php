9bdf99c7662db2b6f8f0255c42a11741a2a6a446
yuki-snow1823 <magic.iroha@gmail.com>
2020-10-23 04:11:19 +0900

    add:子の切り出し

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,14 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
 </head>
+
 <body>
   <h1>test</h1>
   <h2>{{ $todos[0]->title }}</h2>
   <h2>{{ $todos }}</h2>
+
+  {{-- 普通に出力される --}}
+@extends('layouts.footer')
+@section('footer')
+<footer>
+    フッター
+</footer>
+@endsection
+
 </body>
+
 </html>
